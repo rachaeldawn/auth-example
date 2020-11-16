d810b53548251383c3d6619b3e440d17a9746bda
Jonathan Schmold <me@jonathanschmold.ca>
2020-11-16 16:48:18 -0700

    Prettier code

--- src/registration/registration.controller.ts
+++ src/registration/registration.controller.ts
@@ -14,23 +14,20 @@ export class RegistrationController {
 
   @Post()
   public async register(
-    @Body() body: RegisterAccount
+    @Body() { organization, ... body }: RegisterAccount
   ): Promise<MessageResponse> {
     const existing = await this.users.getUser({ email: body.email });
     if (existing?.confirmedAt) {
       throw new ConflictException('Email has already been registered, please log in.')
     }
 
-    const user = await this.users.createUser({
-      age: body.age,
-      email: body.email,
-      name: body.name,
-      password: body.password,
-    });
+    const user = await this.users.createUser(body);
 
     await this.users.createConfirmation(user);
-    const org = await this.orgs.create({ name: body.organization ?? '' }, user.id);
+    const org = await this.orgs.create({ name: organization ?? '' }, user.id);
     await this.orgs.addUser({ userId: user.id, orgId: org.id, role: 'owner' });
+
+    // send the confirmation email here
 
     const msg = 'Registration successful, please check your email to continue';
     return new MessageResponse(msg)
