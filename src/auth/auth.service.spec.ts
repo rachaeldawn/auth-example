4b96e6896974af3647502609fbbd58bf233bf679
Jonathan Schmold <me@jonathanschmold.ca>
2020-11-20 12:11:46 -0700

    fixed unit tests to not fail because of missing things

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -2,16 +2,31 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { OrganizationUserModel } from '@app/organization/models/organization-user.model';
+import { RefreshTokenModel } from './models/refresh-token.model';
+import { PUBLIC_KEY, PRIVATE_KEY } from './auth.constants';
+import { UserService } from '@app/user/user.service';
+import { ConfigService } from '@app/shared/config.service';
+import { SpyObject, createSpyObject } from '@app/testing/spy-object';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let conf: SpyObject<ConfigService>;
 
   beforeEach(async () => {
+
+    conf = createSpyObject(['get']);
+    conf.get.mockReturnValue('');
+
     const module: TestingModule = await Test
       .createTestingModule({
         providers: [
           AuthService,
+          { provide: PUBLIC_KEY, useValue: '' },
+          { provide: PRIVATE_KEY, useValue: '' },
           { provide: getRepositoryToken(OrganizationUserModel), useValue: {} },
+          { provide: getRepositoryToken(RefreshTokenModel), useValue: {} },
+          { provide: UserService, useValue: {} },
+          { provide: ConfigService, useValue: conf },
         ],
       })
       .compile();
--- src/shared/config.service.spec.ts
+++ src/shared/config.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigService } from './config.service';
+import { CONFIG } from './constants';
+import { v4 as uuid } from 'uuid';
 
 describe('ConfigService', () => {
   let service: ConfigService;
+  let conf: any;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ConfigService],
-    }).compile();
+
+    conf = { WORKS: uuid() };
+
+    const module: TestingModule = await Test
+      .createTestingModule({
+        providers: [
+          { provide: CONFIG, useValue: conf },
+          ConfigService,
+        ],
+      })
+      .compile();
 
     service = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get the proper value of a key', () => {
+    const val = service.get('WORKS');
+    expect(val).toEqual(conf.WORKS);
+  });
+
+  it('should get a null if not found', () => {
+    const val = service.get('DOESNOTWORK');
+    expect(val).toEqual(null);
   });
 });
